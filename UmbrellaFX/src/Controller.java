f2bf4cc40bcb1edd110a6c66fe7c8b0c2985226b
mgiap <136319222+mgiap@users.noreply.github.com>
2024-12-22 13:30:56 +0100

    new push method

--- UmbrellaFX/src/Controller.java
+++ UmbrellaFX/src/Controller.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import PlayerImplementation.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -16,9 +16,6 @@ public class Controller {
 
     @FXML
     private Circle cir00, cir01, cir02, cir03, cir10, cir11, cir12, cir13, cir20, cir21, cir22, cir23, cir30, cir31, cir32, cir33;
-
-    @FXML
-    private ChoiceBox<Integer> rowColumnChoiceBox;
 
     @FXML
     private VBox topRedToken, topBlueToken, topGreenToken, topYellowToken;
@@ -44,26 +41,120 @@ public class Controller {
     @FXML
     private Label score0, score1, score2, score3;
 
+    @FXML
+    private Button pushBottom1, pushBottom2, pushBottom3, pushBottom4;
+    @FXML
+    private Button pushLeft1, pushLeft2, pushLeft3, pushLeft4;
+    @FXML
+    private Button pushRight1, pushRight2, pushRight3, pushRight4;
+    @FXML
+    private Button pushTop1, pushTop2, pushTop3, pushTop4;
+
     private Color selectedColor;
     private VBox selectedToken;
     private String selectedSide;
+    private int selectedIndex = -1;
 
     Player player1 = new Player("Giap", 2);
 
     @FXML
     void initialize() {
-
         // Set the board
         setBoard(player1.getBoard().getBoard());
         initializeTokenBoards(player1);
 
-        // Initialize the row/column choice box
-        rowColumnChoiceBox.getItems().addAll(1, 2, 3, 4);
-        rowColumnChoiceBox.setValue(1); // Default value
-
         // Initialize the scoreboard
         List<List<String>> scoreboard = player1.getScoreBoard().getScoreBoard();
         initializeScoreboard(scoreboard);
+
+        // Hide all push buttons initially
+        hideAllPushButtons();
+
+        // Add event handlers to push buttons
+        pushTop1.setOnAction(this::handlePushTop1);
+        pushTop2.setOnAction(this::handlePushTop2);
+        pushTop3.setOnAction(this::handlePushTop3);
+        pushTop4.setOnAction(this::handlePushTop4);
+        pushLeft1.setOnAction(this::handlePushLeft1);
+        pushLeft2.setOnAction(this::handlePushLeft2);
+        pushLeft3.setOnAction(this::handlePushLeft3);
+        pushLeft4.setOnAction(this::handlePushLeft4);
+        pushRight1.setOnAction(this::handlePushRight1);
+        pushRight2.setOnAction(this::handlePushRight2);
+        pushRight3.setOnAction(this::handlePushRight3);
+        pushRight4.setOnAction(this::handlePushRight4);
+        pushBottom1.setOnAction(this::handlePushBottom1);
+        pushBottom2.setOnAction(this::handlePushBottom2);
+        pushBottom3.setOnAction(this::handlePushBottom3);
+        pushBottom4.setOnAction(this::handlePushBottom4);
+    }
+
+    private void handlePushTop1(ActionEvent event) {
+        setSelectedIndex(0);
+    }
+
+    private void handlePushTop2(ActionEvent event) {
+        setSelectedIndex(1);
+    }
+
+    private void handlePushTop3(ActionEvent event) {
+        setSelectedIndex(2);
+    }
+
+    private void handlePushTop4(ActionEvent event) {
+        setSelectedIndex(3);
+    }
+
+    private void handlePushLeft1(ActionEvent event) {
+        setSelectedIndex(0);
+    }
+
+    private void handlePushLeft2(ActionEvent event) {
+        setSelectedIndex(1);
+    }
+
+    private void handlePushLeft3(ActionEvent event) {
+        setSelectedIndex(2);
+    }
+
+    private void handlePushLeft4(ActionEvent event) {
+        setSelectedIndex(3);
+    }
+
+    private void handlePushRight1(ActionEvent event) {
+        setSelectedIndex(0);
+    }
+
+    private void handlePushRight2(ActionEvent event) {
+        setSelectedIndex(1);
+    }
+
+    private void handlePushRight3(ActionEvent event) {
+        setSelectedIndex(2);
+    }
+
+    private void handlePushRight4(ActionEvent event) {
+        setSelectedIndex(3);
+    }
+
+    private void handlePushBottom1(ActionEvent event) {
+        setSelectedIndex(0);
+    }
+
+    private void handlePushBottom2(ActionEvent event) {
+        setSelectedIndex(1);
+    }
+
+    private void handlePushBottom3(ActionEvent event) {
+        setSelectedIndex(2);
+    }
+
+    private void handlePushBottom4(ActionEvent event) {
+        setSelectedIndex(3);
+    }
+
+    private void setSelectedIndex(int index) {
+        selectedIndex = index;
     }
 
     private void initializeTokenBoards(Player player) {
@@ -71,20 +162,20 @@ public class Controller {
         Map<String, Integer> bottomTokens = player.getBottomBoard().getColorCount();
         Map<String, Integer> leftTokens = player.getLeftBoard().getColorCount();
         Map<String, Integer> rightTokens = player.getRightBoard().getColorCount();
-    
+
         initializeTokenBoard(topTokens, topRedToken, topRedLabel, topBlueToken, topBlueLabel, topGreenToken, topGreenLabel, topYellowToken, topYellowLabel);
         initializeTokenBoard(bottomTokens, bottomRedToken, bottomRedLabel, bottomBlueToken, bottomBlueLabel, bottomGreenToken, bottomGreenLabel, bottomYellowToken, bottomYellowLabel);
         initializeTokenBoard(leftTokens, leftRedToken, leftRedLabel, leftBlueToken, leftBlueLabel, leftGreenToken, leftGreenLabel, leftYellowToken, leftYellowLabel);
         initializeTokenBoard(rightTokens, rightRedToken, rightRedLabel, rightBlueToken, rightBlueLabel, rightGreenToken, rightGreenLabel, rightYellowToken, rightYellowLabel);
     }
-    
+
     private void initializeTokenBoard(Map<String, Integer> tokens, VBox redToken, Label redLabel, VBox blueToken, Label blueLabel, VBox greenToken, Label greenLabel, VBox yellowToken, Label yellowLabel) {
         setTokenQuantity(tokens.getOrDefault("R", 0), redToken, redLabel);
         setTokenQuantity(tokens.getOrDefault("B", 0), blueToken, blueLabel);
         setTokenQuantity(tokens.getOrDefault("G", 0), greenToken, greenLabel);
         setTokenQuantity(tokens.getOrDefault("Y", 0), yellowToken, yellowLabel);
     }
-    
+
     private void setTokenQuantity(int quantity, VBox token, Label label) {
         if (quantity > 0) {
             label.setText(String.valueOf(quantity));
@@ -103,9 +194,9 @@ public class Controller {
             {token20, token21, token22},
             {token30, token31, token32}
         };
-    
+
         Label[] scoreLabels = {score0, score1, score2, score3};
-    
+
         for (int i = 0; i < scoreboard.size(); i++) {
             List<String> grid = scoreboard.get(i);
             for (int j = 0; j < grid.size(); j++) {
@@ -115,7 +206,7 @@ public class Controller {
             for (int j = grid.size(); j < tokens[i].length; j++) {
                 tokens[i][j].setVisible(false);
             }
-    
+
             // Set the score label based on the number of colors in the grid
             if (grid.size() == 2) {
                 scoreLabels[i].setText("+3");
@@ -138,6 +229,9 @@ public class Controller {
         selectedColor = (Color) circle.getFill();
         selectedSide = getSide(selectedToken);
         System.out.println("Selected color: " + selectedColor + " from " + selectedSide);
+
+        // Show the appropriate push buttons
+        showPushButtons(selectedSide);
     }
 
     private String getSide(VBox token) {
@@ -153,6 +247,55 @@ public class Controller {
         return "";
     }
 
+    private void showPushButtons(String side) {
+        hideAllPushButtons();
+        switch (side) {
+            case "top":
+                pushTop1.setVisible(true);
+                pushTop2.setVisible(true);
+                pushTop3.setVisible(true);
+                pushTop4.setVisible(true);
+                break;
+            case "left":
+                pushLeft1.setVisible(true);
+                pushLeft2.setVisible(true);
+                pushLeft3.setVisible(true);
+                pushLeft4.setVisible(true);
+                break;
+            case "right":
+                pushRight1.setVisible(true);
+                pushRight2.setVisible(true);
+                pushRight3.setVisible(true);
+                pushRight4.setVisible(true);
+                break;
+            case "bottom":
+                pushBottom1.setVisible(true);
+                pushBottom2.setVisible(true);
+                pushBottom3.setVisible(true);
+                pushBottom4.setVisible(true);
+                break;
+        }
+    }
+
+    private void hideAllPushButtons() {
+        pushTop1.setVisible(false);
+        pushTop2.setVisible(false);
+        pushTop3.setVisible(false);
+        pushTop4.setVisible(false);
+        pushLeft1.setVisible(false);
+        pushLeft2.setVisible(false);
+        pushLeft3.setVisible(false);
+        pushLeft4.setVisible(false);
+        pushRight1.setVisible(false);
+        pushRight2.setVisible(false);
+        pushRight3.setVisible(false);
+        pushRight4.setVisible(false);
+        pushBottom1.setVisible(false);
+        pushBottom2.setVisible(false);
+        pushBottom3.setVisible(false);
+        pushBottom4.setVisible(false);
+    }
+
     @FXML
     void onConfirmPush(ActionEvent event) {
         if (selectedColor == null) {
@@ -160,24 +303,27 @@ public class Controller {
             return;
         }
 
-        int index = rowColumnChoiceBox.getValue() - 1;
+        if (selectedIndex < 0 || selectedIndex > 3) {
+            System.out.println("Invalid index: " + selectedIndex);
+            return;
+        }
 
         switch (selectedSide) {
             case "top":
-                pushTop(index);
-                player1.addFromTop(convertColorToString(selectedColor), index + 1);
+                pushTop(selectedIndex);
+                player1.addFromTop(convertColorToString(selectedColor), selectedIndex + 1);
                 break;
             case "left":
-                pushLeft(index);
-                player1.addFromLeft(convertColorToString(selectedColor), index + 1);
+                pushLeft(selectedIndex);
+                player1.addFromLeft(convertColorToString(selectedColor), selectedIndex + 1);
                 break;
             case "right":
-                pushRight(index);
-                player1.addFromRight(convertColorToString(selectedColor), index + 1);
+                pushRight(selectedIndex);
+                player1.addFromRight(convertColorToString(selectedColor), selectedIndex + 1);
                 break;
             case "bottom":
-                pushBottom(index);
-                player1.addFromBottom(convertColorToString(selectedColor), index + 1);
+                pushBottom(selectedIndex);
+                player1.addFromBottom(convertColorToString(selectedColor), selectedIndex + 1);
                 break;
             default:
                 System.out.println("Invalid side");
@@ -187,6 +333,9 @@ public class Controller {
         reduceTokenQuantity(selectedToken);
 
         player1.getBoard().printBoard();
+
+        // Hide all push buttons after the push
+        hideAllPushButtons();
     }
 
     private void pushLeft(int row) {
