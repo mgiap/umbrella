f8a7483c3a7ddaaa307917f209048567673bf023
Phu Nguyen <162474701+PhuXuan0511@users.noreply.github.com>
2024-12-23 05:50:12 +0700

    Update Main.java

--- UmbrellaFX/src/Main.java
+++ UmbrellaFX/src/Main.java
@@ -7,14 +7,31 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainscene.fxml"));
-        Parent root = loader.load();
+    public void start(Stage primaryStage) {
+        try {
+            // Load IntroScene
+            FXMLLoader introLoader = new FXMLLoader(getClass().getResource("IntroScreen.fxml"));
+            Parent introRoot = introLoader.load();
+            Scene introScene = new Scene(introRoot);
 
-        // Show the scene
-        primaryStage.setTitle("Player Board");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+            // Load MainScene
+            FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("mainscene.fxml"));
+            Parent mainRoot = mainLoader.load();
+            Scene mainScene = new Scene(mainRoot);
+
+            // Set up IntroController with access to stage and main scene
+            IntroController introController = introLoader.getController();
+            introController.setStageAndMainScene(primaryStage, mainScene);
+
+            // Set initial scene to the intro scene
+            primaryStage.setTitle("Umbrella Board Game");
+            primaryStage.setScene(introScene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("");
+        }
     }
 
     public static void main(String[] args) {
